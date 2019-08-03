d60bee571b9930d46809e6ebcc15f58f4b1dfd31
Vasundhara kannan <vasundharakannan65@gmail.com>
2019-08-03 13:47:02 +0530

    Update largest among three numbers.py

--- largest among three numbers.py
+++ largest among three numbers.py
@@ -1,7 +1,7 @@
 a=int(input("Enter the number:"))
 b=int(input("Enter the number:"))
 c=int(input("Enter the number:"))
-if a>b and a>c:
+if a>b and a>c: # Logical op. "and" used to print only if both conditions are true
     print("a is greater")
 elif b>c and b>a:
     print("b is greater")
