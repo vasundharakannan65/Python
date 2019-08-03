ab67ac6f9184be848f538cd952a455d72c130310
Vasundhara kannan <vasundharakannan65@gmail.com>
2019-08-03 10:43:42 +0530

    Create PalindromeOrNot.py

---
+++ PalindromeOrNot.py
@@ -0,0 +1,7 @@
+string1=str(input("Enter the string:"))
+string2=n[::-1] #gives string in a reverse manner
+if string1==string2:
+    print("%s is a palindrome"%string1)
+else:
+    print("%s is not a palindrome"%string1)
+
