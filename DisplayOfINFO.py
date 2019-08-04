367bb9c7cf4f5f6a67c1033af524c7dc3bd4c77e
Vasundhara kannan <vasundharakannan65@gmail.com>
2019-08-04 12:34:20 +0530

    Create DisplayOfINFO.py

---
+++ DisplayOfINFO.py
@@ -0,0 +1,12 @@
+def myfunction(name,age,address):
+    return name
+    return age
+    return address
+name=str(input("Enter the NAME:"))
+age=int(input("Enter the AGE:"))
+address=str(input("Enter the ADDRESS:"))
+myfunction(name,age,address)
+print('NAME:%s'%name)
+print('AGE:%d'%age)
+print('ADDRESS:%s'%address)
+
