e78b85b68a4d783862a53df0de75e6ad6de822a0
Vasundhara kannan <vasundharakannan65@gmail.com>
2019-08-05 17:21:24 +0530

    Update TwoIntegerAreEqual.py

--- TwoIntegerAreEqual.py
+++ TwoIntegerAreEqual.py
@@ -1,9 +1,6 @@
-def func(a,b):
-    if a==b:
-        return "true"
-    else:
-        return c
 a=int(input("Enter the NUM:"))
 b=int(input("Enter the NUM:"))
-c=a+b
-print(func(a,b))
+if a==b or abs(a-b)==5 or (a+b)==5:
+    print('true')
+else:
+    print('false')
