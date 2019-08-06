32dbf9f7ea2573e97ef8968d4ff52e29313cb315
Vasundhara kannan <vasundharakannan65@gmail.com>
2019-08-06 20:18:58 +0530

    Create SENTENCEintoUPPERCASE.py

---
+++ SENTENCEintoUPPERCASE.py
@@ -0,0 +1,5 @@
+#Sentence into uppercase
+a='''Write a Python program to find the number of notes (Sample of notes: 10, 20, 50, 100, 200 and 500 )
+against an given amount.Range-Number of notes(n) : n (1 = n = 1000000).'''
+b=a.upper()
+print(b)
