7f0018c6d258a4cf64e183107569f8de3bfc0eed
Vasundhara kannan <vasundharakannan65@gmail.com>
2019-08-09 16:44:32 +0530

    Create Pattern4.py

---
+++ Pattern4.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the no. of rows:"))
+for i in range(n,0,-1):
+    for j in range(0,n-i):
+        print(end="  ")
+    for j in range(0,i):
+        print("*",end="   ")
+    print("\r")
