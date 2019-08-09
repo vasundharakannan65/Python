76b0dbe97cb186408c05cfb5433e75b244342573
Vasundhara kannan <vasundharakannan65@gmail.com>
2019-08-09 18:39:03 +0530

    Create GETandPRINTstring.py

---
+++ GETandPRINTstring.py
@@ -0,0 +1,10 @@
+class IOString():
+    def get_String(self):
+        self.str1 = input()
+
+    def print_String(self):
+        print(self.str1)
+
+str1 = IOString()
+str1.get_String()
+str1.print_String()
