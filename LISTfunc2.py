dd838e9179a02efbe0e583f0bd380f543dcdb757
Vasundhara kannan <vasundharakannan65@gmail.com>
2019-08-10 10:45:13 +0530

    Create LISTfunc2.py

---
+++ LISTfunc2.py
@@ -0,0 +1,8 @@
+def func():
+    l=list()
+    for x in range(1,21):
+        l.append(x**2)
+    print(l)
+    print(l[:5])#To print first 5 elements
+    print(l[-5:])#To print last 5 elements
+func()
